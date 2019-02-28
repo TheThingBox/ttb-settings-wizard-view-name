89317fb08af94d78cfa061e7b9295d243e578d1d
lucaspouchot <lucas.pouchot@gmail.com>
2019-02-28 14:08:59 +0100

    update syncStats

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const path = require('path')
 const fs = require('fs')
+const interface_utils = require('ttbd-interface-utils')({hydra_exec_host: "mosquitto"})
 
 const name = 'name'
 var settingsPath = null
@@ -20,7 +21,7 @@ function init(app, apiToExpose, persistenceDir) {
   settingsPath = path.join(settingsPath, 'settings.json')
   syncStats()
 
-  app.use(apiToExpose, bodyParser.urlencoded({ extended: true }));
+  app.use(apiToExpose, bodyParser.json());
   app.get(apiToExpose, function(req, res){
     syncStats()
     res.json(stats)
@@ -32,18 +33,24 @@ function init(app, apiToExpose, persistenceDir) {
   });
 }
 
-function syncStats(){
+function syncStats(update){
   if(!settingsPath){
     return
   }
+  var statsFromFile
   try {
-    stats = JSON.parse(fs.readFileSync(settingsPath))
+    statsFromFile = JSON.parse(fs.readFileSync(settingsPath))
+    if(update === true){
+      stats = Object.assign({}, statsFromFile, stats)
+    } else {
+      stats = Object.assign({}, stats, statsFromFile)
+    }
   } catch(e){
     try {
       fs.writeFileSync(settingsPath, JSON.stringify(stats, null, 4), { encoding: 'utf8'})
     } catch(e){}
   }
-  if(stats.initialized === false){
+  if(stats.initialized === false || update === true){
     stats.initialized = true
     try {
       fs.writeFileSync(settingsPath, JSON.stringify(stats, null, 4), { encoding: 'utf8'})
@@ -59,6 +66,6 @@ module.exports = {
   init: init,
   getStats: getStats,
   syncStats: syncStats,
-  order: 30,
+  order: 20,
   canIgnore: true
 }
